31522668ebfca6bc4d62ce27fe272465f0233d40
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-02-09 18:15:38 -0300

    Setters, Getters and Constructors created using lombok

--- src/main/java/com/rodrigocandidodev/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/rodrigocandidodev/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
@@ -3,6 +3,13 @@ package com.rodrigocandidodev.certification_nlw.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnswersCertificationsEntity{
 	private UUID id;
 	private UUID certificationID;
